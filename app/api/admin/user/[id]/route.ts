d5f8d2f7fc6e6cdf9b2b01a4874bab18a5a2f021
ilyaasomar <ilyaascumar10@gmail.com>
2024-03-17 10:22:19 +0300

    stripe starting

--- app/api/admin/user/[id]/route.ts
+++ app/api/admin/user/[id]/route.ts
@@ -1,10 +1,22 @@
+import { AuthOptions } from "@/app/api/auth/[...nextauth]/AuthOptions";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-
+type User = {
+  role: string;
+};
 export async function PUT(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(AuthOptions);
+
+  const userRole = (session?.user as User).role;
+
+  if (userRole !== "admin") {
+    return NextResponse.json("un authorized", { status: 401 });
+  }
+
   const body = await request.json();
 
   let updatedUser = await prisma.user.update({
---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+type User = {
+  role: string;
+};
+
+export const config = {
+  matcher: ["/dashboard/user:path*", "/dashboard/admin:path*"],
+};
+
+export default withAuth(
+  async function middleware(req) {
+    const url = req.nextUrl.pathname;
+
+    const userRole = (req?.nextauth?.token?.user as User).role;
+
+    if (url?.includes("/admin") && userRole !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => {
+        return !!token;
+      },
+    },
+  }
+);
